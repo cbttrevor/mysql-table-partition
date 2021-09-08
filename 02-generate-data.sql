68a7f21993eecdd2ac14344600f8fb8ea63c1fec
Trevor Sullivan <trevor@trevorsullivan.net>
2021-09-08 11:30:31 -0600

    Update 02-generate-data.sql

--- 02-generate-data.sql
+++ 02-generate-data.sql
@@ -27,3 +27,18 @@ CREATE PROCEDURE cbtpart.addRows () MODIFIES SQL DATA
   END//
 DELIMITER ;
 CALL cbtpart.addRows();
+
+-- Insert Zimbabwe location
+DROP PROCEDURE IF EXISTS cbtpart.addRows;
+DELIMITER //
+CREATE PROCEDURE cbtpart.addRows () MODIFIES SQL DATA
+  BEGIN
+    SET @current_count = 0;
+    WHILE @current_count < 300 DO
+      INSERT INTO cbtpart.races (firstName, timeInSeconds, location) VALUES
+        ROW('Trevor', 200, 'Zimbabwe');
+      SET @current_count = @current_count + 1;
+    END WHILE;
+  END//
+DELIMITER ;
+CALL cbtpart.addRows();
