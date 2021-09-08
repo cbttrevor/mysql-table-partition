543e289065802df0607e44126a0eb717a6b432a6
Trevor Sullivan <trevor@trevorsullivan.net>
2021-09-08 11:24:14 -0600

    Create 02-generate-data.sql

---
+++ 02-generate-data.sql
@@ -0,0 +1,29 @@
+-- Insert Denver location
+DROP PROCEDURE IF EXISTS cbtpart.addRows;
+DELIMITER //
+CREATE PROCEDURE cbtpart.addRows () MODIFIES SQL DATA
+  BEGIN
+    SET @current_count = 0;
+    WHILE @current_count < 300 DO
+      INSERT INTO cbtpart.races (firstName, timeInSeconds, location) VALUES
+        ROW('Trevor', 200, 'Denver');
+      SET @current_count = @current_count + 1;
+    END WHILE;
+  END//
+DELIMITER ;
+CALL cbtpart.addRows();
+
+-- Insert San Francisco location
+DROP PROCEDURE IF EXISTS cbtpart.addRows;
+DELIMITER //
+CREATE PROCEDURE cbtpart.addRows () MODIFIES SQL DATA
+  BEGIN
+    SET @current_count = 0;
+    WHILE @current_count < 300 DO
+      INSERT INTO cbtpart.races (firstName, timeInSeconds, location) VALUES
+        ROW('Trevor', 200, 'San Francisco');
+      SET @current_count = @current_count + 1;
+    END WHILE;
+  END//
+DELIMITER ;
+CALL cbtpart.addRows();
